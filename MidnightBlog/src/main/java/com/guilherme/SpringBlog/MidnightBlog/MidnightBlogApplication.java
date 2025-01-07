722ec3196663c2f53fd795e27f8ab9fd338da014
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2025-01-07 18:06:19 -0300

    The project has been created

---
+++ MidnightBlog/src/main/java/com/guilherme/SpringBlog/MidnightBlog/MidnightBlogApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.SpringBlog.MidnightBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidnightBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidnightBlogApplication.class, args);
+	}
+
+}
---
+++ MidnightBlog/src/test/java/com/guilherme/SpringBlog/MidnightBlog/MidnightBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.SpringBlog.MidnightBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidnightBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
