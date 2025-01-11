399358492d26ca3d6a848a0ec8cdf2ecec8fc8a4
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2025-01-11 19:02:02 -0300

    The UserDTO has been added

--- MidnightBlog/src/main/java/com/guilherme/SpringBlog/MidnightBlog/resources/UserResource.java
+++ MidnightBlog/src/main/java/com/guilherme/SpringBlog/MidnightBlog/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.guilherme.SpringBlog.MidnightBlog.resources;
 
 import com.guilherme.SpringBlog.MidnightBlog.domain.User;
+import com.guilherme.SpringBlog.MidnightBlog.dto.UserDTO;
 import com.guilherme.SpringBlog.MidnightBlog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,10 +22,10 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> users = userService.findAll();
-
-        return ResponseEntity.ok().body(users);
+        List<UserDTO> userDTOS = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOS);
     }
 
 }
