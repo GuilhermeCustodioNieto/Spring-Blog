77b68a821c56e8fdeab9ea9e9dd358cc2f531d4b
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2025-01-11 20:19:01 -0300

    The user post has been added

--- MidnightBlog/src/main/java/com/guilherme/SpringBlog/MidnightBlog/services/UserService.java
+++ MidnightBlog/src/main/java/com/guilherme/SpringBlog/MidnightBlog/services/UserService.java
@@ -1,6 +1,7 @@
 package com.guilherme.SpringBlog.MidnightBlog.services;
 
 import com.guilherme.SpringBlog.MidnightBlog.domain.User;
+import com.guilherme.SpringBlog.MidnightBlog.dto.UserDTO;
 import com.guilherme.SpringBlog.MidnightBlog.repository.UserRepository;
 import com.guilherme.SpringBlog.MidnightBlog.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO userDTO){
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
