87754e318e102e06e47c22f77a4b2309480f6d1f
Guilherme Custódio Nieto <guilhermecustodionieto@gmail.com>
2025-01-14 14:15:36 -0300

    Query method has been added

--- MidnightBlog/src/main/java/com/guilherme/SpringBlog/MidnightBlog/repository/PostRepository.java
+++ MidnightBlog/src/main/java/com/guilherme/SpringBlog/MidnightBlog/repository/PostRepository.java
@@ -2,10 +2,15 @@ package com.guilherme.SpringBlog.MidnightBlog.repository;
 
 import com.guilherme.SpringBlog.MidnightBlog.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 
 public interface PostRepository extends MongoRepository<Post, String> {
-    List<Post> findByTitleContaining(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
